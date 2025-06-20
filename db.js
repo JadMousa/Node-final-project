a32851f4248acf688c33aa7817162b4b345c2c09
JadMousa <jadmousa12@gmail.com>
2025-06-20 16:21:33 +0300

    Improving the database

--- db.js
+++ db.js
@@ -7,6 +7,9 @@ const { Pool } = pkg;
 
 const pgclient = new Pool({
   connectionString: process.env.DATABASE_URL, // or use user, password, host, etc.
+  ssl: {
+    rejectUnauthorized: false // allows self-signed certs
+  }
 });
 
 export default pgclient;
