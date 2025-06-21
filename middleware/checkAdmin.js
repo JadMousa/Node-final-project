5c92bc0ede8262f5d1ddd4e2871568142043538d
JadMousa <jadmousa12@gmail.com>
2025-06-21 11:24:52 +0300

    Testing CheckAdmin for deployment

--- middleware/checkAdmin.js
+++ middleware/checkAdmin.js
@@ -1,5 +1,6 @@
 const checkAdmin = (req, res, next) => {
     const userEmail = req.headers['user-email']; // sent from frontend Auth0
+    console.log("Checking admin access. Received email:", userEmail); // Log incoming header
     if (userEmail === "jadmousa12@gmail.com") {
         next();
     } else {
