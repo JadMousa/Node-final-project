a7a0126f77cfe8fac3ec1a18238d40b519c295a4
JadMousa <jadmousa12@gmail.com>
2025-06-21 11:58:41 +0300

    Fixing problem in allowed headers

--- server.js
+++ server.js
@@ -13,23 +13,13 @@ const allowedOrigins = [
     'https://react-final-project-production-2603.up.railway.app',
     'http://localhost:3000'
 ];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            console.log('Blocked by CORS:', origin); // Helpful for debugging
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Explicit methods
-    allowedHeaders: ['Content-Type', 'Authorization'] // Explicit headers
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'user-email']
 }));
+
 
 app.use(express.json());
 
